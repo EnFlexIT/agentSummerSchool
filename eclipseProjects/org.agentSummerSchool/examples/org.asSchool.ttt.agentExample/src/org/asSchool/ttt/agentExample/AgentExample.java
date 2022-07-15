b93b30fc267a4f5eff7f6c8e8948453b81b2625d
Maximilian Kilthau <maximilian.kilthau@hsu-hh.de>
2022-07-15 11:12:24 +0200

    New Agent Project

---
+++ eclipseProjects/org.agentSummerSchool/examples/org.asSchool.ttt.agentExample/src/org/asSchool/ttt/agentExample/AgentExample.java
@@ -0,0 +1,24 @@
+package org.asSchool.ttt.agentExample;
+
+
+import jade.content.lang.sl.SLCodec;
+import jade.core.AID;
+import jade.core.Agent;
+
+public class AgentExample extends Agent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private AID gameMasterAID;
+	private SLCodec slCodec;
+
+	protected void setup() {
+		
+		
+	
+	}
+	
+	
+}
---
+++ eclipseProjects/org.agentSummerSchool/examples/org.asSchool.ttt.agentExample/src/org/asSchool/ttt/agentExample/behaviour/RegisterBehaviourExample.java
@@ -0,0 +1,26 @@
+package org.asSchool.ttt.agentExample.behaviour;
+
+import org.asSchool.ttt.agentExample.AgentExample;
+import org.asSchool.ttt.dataModel.ontology.AbstractPlayer;
+import org.asSchool.ttt.dataModel.ontology.Register;
+
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
+
+public class RegisterBehaviourExample extends OneShotBehaviour {
+
+	private static final long serialVersionUID = -5983918484647149434L;
+
+	
+	public RegisterBehaviourExample() {
+		
+	}
+	
+	/* (non-Javadoc)
+	 * @see jade.core.behaviours.Behaviour#action()
+	 */
+	@Override
+	public void action() {
+		
+	}
+}
